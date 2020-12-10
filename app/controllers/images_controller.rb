594db288c67ccfd33fbbb1f52d3e75f15c9398b5
handcrab <hancrab@gmail.com>
2020-12-10 03:45:10 +0300

    create images controller

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,9 @@
+class ImagesController < ApplicationController
+    def compress
+      head :ok
+    end
+  
+    def download
+      head :ok
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :images, only: :none do
+    post :compress, on: :collection
+    get :download, on: :member
+  end
+
+  if Rails.env.development?
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
